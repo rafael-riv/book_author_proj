565d15045ab58d5270d8d1984cf12415cd7b808f
juan_diaz <juan.diazr@usach.cl>
2021-08-11 18:51:45 -0400

    Consultas antes del ManytoMany

---
+++ books_authors_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import CharField
+
+# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=255)
+    desc  = models.TextField()
+    create_ad = models.DateTimeField(auto_now_add= True )
+    update_ad = models.DateTimeField(auto_now_add= True)
+    def __repr__(self) -> str:
+        return f'{self.id} : {self.title}'
+
+class Author(models.Model):
+    firt_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    notas = models.TextField(default='hola')
+    create_ad = models.DateTimeField(auto_now_add= True )
+    update_ad = models.DateTimeField(auto_now_add= True)
+    def __repr__(self) -> str:
+        return f'{self.id} : {self.firt_name}'
