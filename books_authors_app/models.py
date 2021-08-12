947c9b73161de4dc0e8008c31806b12028bde283
juan_diaz <juan.diazr@usach.cl>
2021-08-12 18:41:06 -0400

    Consultas completas

--- books_authors_app/models.py
+++ books_authors_app/models.py
@@ -8,6 +8,7 @@ class Books(models.Model):
     desc  = models.TextField()
     create_ad = models.DateTimeField(auto_now_add= True )
     update_ad = models.DateTimeField(auto_now_add= True)
+    # authors
     def __repr__(self) -> str:
         return f'{self.id} : {self.title}'
 
@@ -15,6 +16,7 @@ class Author(models.Model):
     firt_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     notas = models.TextField(default='hola')
+    books = models.ManyToManyField(Books, related_name="authors")
     create_ad = models.DateTimeField(auto_now_add= True )
     update_ad = models.DateTimeField(auto_now_add= True)
     def __repr__(self) -> str:
